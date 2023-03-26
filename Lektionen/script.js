3768a14c71d0e2a50ab6d2e5f06e8b43ad0c228c
fuzzy123-ai <79788260+fuzzy123-ai@users.noreply.github.com>
2023-03-26 13:51:22 +0200

    Add files via upload

--- Lektionen/script.js
+++ Lektionen/script.js
@@ -1,7 +1,13 @@
 function zeigeLoesung() {
+  var password = prompt("Bitte geben Sie das Passwort ein:");
+  
+  // Hier können Sie das Passwort anpassen
+  var correctPassword = "0210";
+  
+  if (password === correctPassword) {
     var solution = document.querySelector(".loesung");
     var button = document.querySelector("#toggle-button");
-    
+
     if (solution.style.display === "block") {
       solution.style.display = "none";
       button.innerHTML = "Lösung anzeigen";
@@ -9,5 +15,7 @@ function zeigeLoesung() {
       solution.style.display = "block";
       button.innerHTML = "Lösung verbergen";
     }
+  } else {
+    alert("Falsches Passwort. Bitte versuchen Sie es erneut.");
   }
-  
+}
